8b34ccefcc058bc790c2e9a3d26fac2afc809971
TamanHassan <tamanhassan1@gmail.com>
2026-04-15 11:24:52 +0200

    added frontend

---
+++ Frontend/app.js
@@ -0,0 +1,13 @@
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const response = await fetch("http://localhost:3000/employee/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  });
+
+  const data = await response.json();
+  document.getElementById("result").innerText = JSON.stringify(data, null, 2);
+}
